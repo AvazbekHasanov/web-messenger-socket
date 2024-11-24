ed7779760c1ed1aff129119064a1dd5b0d1eaadc
AvazbekHasanov <avazbekxasanov200@gmail.com>
2024-11-24 21:56:59 +0500

    First commit

---
+++ socket.js
@@ -0,0 +1,58 @@
+const {log} = require("console");
+const express = require("express");
+const app = express();
+const http = require("http");
+const {userInfo} = require("os");
+const server = http.createServer(app);
+const {Server} = require("socket.io");
+const io = new Server(server);
+
+
+const userList = [];
+
+app.get("/", (req, res) => {
+    res.send('<h1>Hello world</h1>')
+});
+
+io.on("connection", (socket) => {
+    console.log("a user connected", socket.id);
+    socket.on("connectedNewUser", (data) => {
+        const index = userList.findIndex((el) => el.id === data.id);
+
+        if (index >= 0) {
+            userList[index] = {...data, socketId: socket.id}
+            console.log("enter if", index);
+
+        } else {
+            userList.push({...data, socketId: socket.id})
+            console.log("enter else", index);
+        }
+
+        console.log("userList", userList);
+
+    });
+
+    socket.on("newMessageSend", (data) => {
+        const sortedUser = []
+        data.members.forEach((member) => {
+            if (userList.some( u => member.user_id == u.id )){
+                sortedUser.push(userList.find(u => member.user_id == u.id  ))
+            }
+        })
+
+        sortedUser.forEach((user) => {
+            console.log("user.socketId", user.socketId)
+            io.to(user.socketId).emit("newMessage", data); // `user.socketId` must be the correct connected Socket.IO ID
+        });
+    });
+
+    socket.on("disconnect", (reason) => {
+        console.log(`Socket disconnected: ${socket.id}, Reason: ${reason}`,
+            userList.find(u => u.socketId = socket.id));
+        userList.splice(userList.findIndex(u => u.socketId = socket.id), 1);
+    });
+});
+
+server.listen(8080, () => {
+    console.log("listening on *:8080");
+});
